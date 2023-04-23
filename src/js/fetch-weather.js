c503577419d478a14e15d5e948bb0acf887f774c
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-04-23 08:36:26 +0300

    add location

--- src/js/fetch-weather.js
+++ src/js/fetch-weather.js
@@ -1,7 +1,7 @@
 const API_WEATHER_KEY = 'a323d6674456ccaaf9a7dc4a714c315f';
 
 export function fetchWeather () {
-
+  
   return fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kharkiv&lang=ru&units=metric&appid=${API_WEATHER_KEY}`).then((response)=> {
     if(response.ok) {
       return response.json()
@@ -10,3 +10,22 @@ export function fetchWeather () {
     }
     })
 };
+
+// динамическое получение координат
+function getGeoLocation () {
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      coordsLocation = {
+        lat,
+        lon
+      }
+
+    });
+  }
+};
+
+
+
