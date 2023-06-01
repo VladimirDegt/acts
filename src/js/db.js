d57c50639f6e7d5cc63ce09cd9c329268abf1ea8
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-06-01 10:16:33 +0300

    +

--- src/js/db.js
+++ src/js/db.js
@@ -1,10 +1,10 @@
 let idNumber = 0;
 export const db = [ 
-  // {
-  // id: `${idNumber += 1}`,
-  // name: 'Окко-Драйв',
-  // document: '№38',
-  // year: 2023,
-  // month: 'квітень',
-  // }, 
+  {
+  id: `${idNumber += 1}`,
+  name: 'Укргазбанк',
+  document: '№47',
+  year: 2023,
+  month: 'квітень',
+  }, 
 ];
